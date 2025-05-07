3f43312f18c27a6abfbca8072b58eed4be7f1a71
Maitri-Rajpara <maitri.rajpara12@gmail.com>
2025-05-07 17:13:25 +0530

    Create AddTask.tsx

---
+++ src/Pages/AddTask.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TaskForm from "../Components/AddEditTask/TaskForm";
+
+export default function AddTask() {
+  return (
+    <div className="task-form-page">
+      <TaskForm />
+    </div>
+  );
+}
