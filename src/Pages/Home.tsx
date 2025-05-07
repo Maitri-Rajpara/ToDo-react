9bb93b1a99c3963009a73553ff288c73499aa988
Maitri-Rajpara <maitri.rajpara12@gmail.com>
2025-05-07 17:13:51 +0530

    Create Home.tsx

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,18 @@
+import TaskList from "../Components/TaskList/TaskList";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+  const navigate = useNavigate();
+  return (
+    <div className="task-manager">
+      <div className="heading">
+        <h2>Task Manager</h2>
+        <button onClick={() => navigate("/add")} className="add-btn-head">
+          Add Task
+        </button>
+      </div> 
+      <TaskList />
+    </div>
+  );
+}
